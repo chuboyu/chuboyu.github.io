2a9bd406631ef24958e7cf4ff2bd516684eba59a
boyu <chuboyu.tw@gmail.com>
2025-11-22 10:15:45 +0800

    All info updates except micro-SaaS

--- calendar.js
+++ calendar.js
@@ -10,7 +10,6 @@
     // Wait for the window to fully load and the Google script to be available
     window.addEventListener('load', function() {
         const buttons = document.querySelectorAll('.book-appointment-button');
-
         buttons.forEach(button => {
             if (window.calendar && window.calendar.schedulingButton) {
                 // Apply the scheduling button to each target element
@@ -22,6 +21,19 @@
                     target: button,
                 });
             }
+        });
+        const visionButtons = document.querySelectorAll('.vision-talk-button');
+        visionButtons.forEach(button => {
+              if (window.calendar && window.calendar.schedulingButton) {
+                  // Apply the scheduling button to each target element
+                  calendar.schedulingButton.load({
+                      // *** YOUR UNIQUE URL AND SETTINGS HERE ***
+                      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3nirHONfHOvuOy1M0z1z5hi7QLwGvcMGeODgtDULztNFR2Xkmm3NELS1G-7DaXgjz8FXIpLXxs?gv=true',
+                      color: '#0B8043',
+                      label: button.getAttribute('data-label') || 'Book an appointment', // Allows overriding the label
+                      target: button,
+                  });
+              }
         });
     });
 })();
